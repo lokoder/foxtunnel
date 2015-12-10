80af8f1344ee0ac55099969a1c8ccf36a6037099
Erick Andrade <erick.andrade@inbox.com>
2015-12-10 18:38:19 -0200

    save...

--- index.js
+++ index.js
@@ -5,17 +5,25 @@ var tabs = require("sdk/tabs");
 var button = buttons.ActionButton({
   id: "mozilla-link",
   label: "Visit Mozilla",
-  icon: {
-    "16": "./icon-16.png",
-    "32": "./icon-32.png",
-    "64": "./icon-64.png"
-  },
+  icon: "./http.png",
   onClick: handleClick
 });
 
 
-var HTTP = 1;
-var SOCKS = 2;
+exports.onUnload = function (reason) {
+
+	if (pid) {
+
+		var child_process = require("sdk/system/child_process");
+
+		console.log('kill -9 no PID ' + pid);
+		var kill = child_process.spawn('/bin/kill', ['-9', ''+ pid]);
+	}
+};
+
+
+
+var PROXY = { HTTP:1, SOCKS:2 };
 
 
 function setProxy(type) {
@@ -24,18 +32,25 @@ function setProxy(type) {
 
 	prefsvc.set("network.proxy.type", 1);
 
-	if (type == HTTP) {
+	console.log("dentro de setProxy");
 
+	if (type == PROXY.HTTP) {
+		
+		console.log("http!!!");
 		prefsvc.set("network.proxy.http", 'webfilter.trtsp.jus.br');
 		prefsvc.set("network.proxy.http_port", 3128);
 		prefsvc.set("network.proxy.share_proxy_settings", true);
+		button.icon = "./http.png";
 
-	} else if (type == SOCKS) {
+	} else if (type == PROXY.SOCKS) {
 
+		console.log("socks!!!");
 		prefsvc.set("network.proxy.socks", 'localhost');
 		prefsvc.set("network.proxy.socks_port", 10000);
 		prefsvc.set("network.proxy.socks_remote_dns", true);
 		prefsvc.set("network.proxy.socks_version", 5);
+		prefsvc.set("network.proxy.share_proxy_settings", false);
+		button.icon = "./socks.png";
 	}
 }
 
@@ -60,6 +75,7 @@ function handleClick(state) {
 		var ls = child_process.spawn('/usr/bin/ssh', ['-N', '-D 10000', 'erick@localhost']);
 		pid = ls.pid;
 		console.log("Criada conexão SSH... PID: " + pid);
+		setProxy(PROXY.SOCKS);
 
 		ls.stderr.on('data', function (data) {
 			console.log('stderr: ' + data);
@@ -68,16 +84,14 @@ function handleClick(state) {
 		ls.on('close', function (code) {
 		  console.log('A conexão SSH foi encerrada: ' + code);
 		  pid = 0;
-		  setProxy(HTTP);
+		  setProxy(PROXY.HTTP);
 		});
 
 		ls.on('error', function(code) {
 		  console.log('ERRO: falha ao executar comando');
 		  pid = 0;
-		  setProxy(HTTP);
+		  setProxy(PROXY.HTTP);
 		});
-	
-		setProxy(SOCKS);
 	}
 
 }
